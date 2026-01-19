b4c29081a5b1e18c574f8ae02b70ee6928523964
thesarikonda <140501081+thesarikonda@users.noreply.github.com>
2026-01-19 19:43:45 +0530

    updated index.js and socket.js

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 /* ---------- MIDDLEWARE ---------- */
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
--- server/src/socket.js
+++ server/src/socket.js
@@ -204,7 +204,7 @@ const setupSocket = (server) => {
 
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.CLIENT_URL,
       methods: ["GET", "POST"],
       credentials: true,
     },
