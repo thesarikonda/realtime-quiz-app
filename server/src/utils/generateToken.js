fb043ca04f12e1e73e03fb0411ed7d3d9b3da438
thesarikonda <140501081+thesarikonda@users.noreply.github.com>
2026-01-19 19:58:34 +0530

    jwt sameSite update

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie( "jwt", token,{
         maxAge : 15 * 24 * 60 * 60 * 1000,
         httpOnly : true, // prevent XSS
-        sameSite : "lax", // prevent CSRF    
+        secure:true,
+        sameSite : "None", //  
         secure: process.env.NODE_ENV !== 'development',
     });
 
